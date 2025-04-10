132f607c32b7f921e002c3b676775424f5e9e366
Reham Abuhadba <c>
2025-04-10 13:24:06 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'sign_in.dart'; // Make sure the file exists
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'firebase_options.dart';
 
 // os_v2_app_2c6m3eqrf5fkpajmcqgdof6nxliov3whl46uxzn3jcwxb43677qp4uq47exfo6qvyhskjrmy3iwzhml2qwd523luq2c4aa5uoviaf3i
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // await Firebase.initializeApp(
-  //   // options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
   OneSignal.initialize("f23e3c05-7911-4b35-a371-075f196c41fd");
